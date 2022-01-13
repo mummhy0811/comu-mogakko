f78231f794969d45d6e62b187e18131155eae899
mummhy0811 <mummhy0811@inu.ac.kr>
2022-01-13 22:38:01 +0900

    day4

--- js_study/js_study.js
+++ js_study/js_study.js
@@ -1,18 +1,20 @@
-function changeText1(){
-    var h1 = document.getElementsByTagName('h1');
-    h1[0].innerHTML = "모여서"
+var count=0;
+function changeColor(){
+    var h1=document.getElementsByTagName('h1');
+    count++;
+    h1[0].innerHTML="짱구에게 하트를 "+count+"번 주었어요.";
+    var img=document.getElementsByTagName('img');
+    if(count%3==0) img[0].src="./character/1.jpg"
+    else if(count%3==1) img[0].src="./character/2.jpg"
+    else if(count%3==2) img[0].src="./character/3.jpg"
+    console.log("짱구에게 하트를 "+count+"번 주었어요.");
 }
-function changeText2(){
-    var h1 = document.getElementsByTagName('h1');
-    h1[1].innerHTML = "각자"
-}
-function changeText3(){
-    var h1 = document.getElementsByTagName('h1');
-    h1[2].innerHTML = "코딩하기!"
-}
-function clearText(){
-    var h1 = document.getElementsByTagName('h1');
-    h1[0].innerHTML = "모"
-    h1[1].innerHTML = "각"
-    h1[2].innerHTML = "코"
+function clearnumber(){
+
+    var h1=document.getElementsByTagName('h1');
+    count=0;
+    h1[0].innerHTML="짱구에게 하트를 0번 주었어요";
+    var img=document.getElementsByTagName('img');
+    img[0].src="./character/1.jpg"
+    console.log("하트 수를 초기화 했어요.");
 }
