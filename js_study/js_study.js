04f482b664e913707b95d58f258a12444e048a41
mummhy0811 <mummhy0811@inu.ac.kr>
2022-01-14 16:36:42 +0900

    day5

--- js_study/js_study.js
+++ js_study/js_study.js
@@ -1,20 +1,30 @@
-var count=0;
-function changeColor(){
-    var h1=document.getElementsByTagName('h1');
-    count++;
-    h1[0].innerHTML="짱구에게 하트를 "+count+"번 주었어요.";
-    var img=document.getElementsByTagName('img');
-    if(count%3==0) img[0].src="./character/1.jpg"
-    else if(count%3==1) img[0].src="./character/2.jpg"
-    else if(count%3==2) img[0].src="./character/3.jpg"
-    console.log("짱구에게 하트를 "+count+"번 주었어요.");
+var n=123456;
+var s="hello";
+var b=true;
+var o={name:"코뮤니티", age: 2};
+var u;
+function number(){
+    console.log(typeof n);
+    var p=document.getElementsByTagName('p')
+    p[0].innerHTML="n의 타입은 \"number\"입니다.  javascript는 다른 언어와 다르게 정수와 실수를 따로 구분하지 않아요!"
 }
-function clearnumber(){
-
-    var h1=document.getElementsByTagName('h1');
-    count=0;
-    h1[0].innerHTML="짱구에게 하트를 0번 주었어요";
-    var img=document.getElementsByTagName('img');
-    img[0].src="./character/1.jpg"
-    console.log("하트 수를 초기화 했어요.");
+function string(){
+    console.log(typeof s);
+    var p=document.getElementsByTagName('p')
+    p[0].innerHTML="s의 타입은 \"string\"입니다. 큰 따옴표나 작은따옴표로 둘러싸인 문자의 잡합을 말해요."
+}
+function boolean(){
+    console.log(typeof b);
+    var p=document.getElementsByTagName('p')
+    p[0].innerHTML="b의 타입은 \"boolean\"입니다. 불리언은 참과 거짓을 표현해요."
+}
+function object(){
+    console.log(typeof o);
+    var p=document.getElementsByTagName('p')
+    p[0].innerHTML="o의 타입은 객체입니다. 객체는 키(key)와 값(value)으로 이루어져 있고, 키를 통해 그 값을 불러올 수 있어요."
+}
+function unDefined(){
+    console.log(typeof u);
+    var p=document.getElementsByTagName('p')
+    p[0].innerHTML="u는 타입이 정해지지 않았기 때문에 \"undefined\"타입입니다. 이와 비슷하게 값이 정해지지 않은 것은 \"null\"이라고 해요."
 }
