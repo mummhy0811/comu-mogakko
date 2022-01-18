2ec57a103a02ca01b46cb845b7608499c24e090f
mummhy0811 <mummhy0811@inu.ac.kr>
2022-01-18 19:05:09 +0900

    refactor: Make stopwatch
    Make stopwatch by using the contents of day7.
    e.g. conditional statements, loops

--- js_study/js_study.js
+++ js_study/js_study.js
@@ -1,29 +1,50 @@
-var x=5, y=3;
-var ans=0;
-function add(){
-    console.log(x+y);
-    var p=document.getElementsByTagName('p');
-    ans=x+y;
-    p[0].innerHTML="x+y="+ans;
+var h=0, m=0, s=0, ms=0;
+var sp=document.getElementsByTagName('span');
+var timer;
+
+function hour(){
+    h=parseInt(sp[0].innerHTML);
+    h++;
+    sp[0].innerHTML=h>=10?h:"0"+h;
 }
-function sub(){
-    console.log(x-y);
-    var p=document.getElementsByTagName('p');
-    ans=x-y;
-    p[0].innerHTML="x-y="+ans;
+function minute(){
+    m=parseInt(sp[1].innerHTML);
+    m++;
+    if(m==60){
+        hour();
+        m=0;
+    }
+    sp[1].innerHTML=m>=10?m:"0"+m;
 }
-function mul(){
-    console.log(x*y);
-    var p=document.getElementsByTagName('p');
-    p[0].innerHTML="x*y="+x*y;
+function second(){
+    s=parseInt(sp[2].innerHTML);
+    s++;
+    if(s==60){
+        minute();
+        s=0;
+    }
+    sp[2].innerHTML=s>=10?s:"0"+s;
 }
-function div1(){
-    console.log(x/y);
-    var p=document.getElementsByTagName('p');
-    p[0].innerHTML="x+y="+x/y;
+function millisec(){
+    ms=parseInt(sp[3].innerHTML);
+    ms++;
+    if(ms==100){
+        second();
+        ms=0;
+    }
+    sp[3].innerHTML=ms>=10?ms:"0"+ms;
 }
-function div2(){
-    console.log(x%y);
-    var p=document.getElementsByTagName('p');
-    p[0].innerHTML="x+y="+x%y;
+
+
+function start(){
+    timer=setInterval(millisec, 10);
+}
+function stop(){
+    clearInterval(timer);
+}
+function reset(){
+    stop();
+    for(var i=0;i<4;i++){
+        sp[i].innerHTML="00";
+    }
 }
