0aa999fe6ca9b3216999968d6e1e6f08bce2fb96
mummhy0811 <mummhy0811@inu.ac.kr>
2022-01-25 21:41:07 +0900

    feat: 챗봇 명령어 반응 추가
    명령어 입력시 css를 이용하여 배경색을 바꾸는 코드추가
    기타 여러가지 명령어 추가

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,8 +1,9 @@
 var p=document.getElementsByTagName('p');
-
-
+var body=document.getElementsByTagName('body');
+var img=document.getElementsByTagName('img');
 function check_text(){
     var value = document.getElementById("console").value;
+	/*
 	if(value == ""){
 		alert("명령어를 입력하세요.");
 	}
@@ -12,5 +13,50 @@ function check_text(){
 	else{
 		p[0].innerHTML=value;
     }
+	*/
+	switch(value){
+		case(""):
+			alert("명령어를 입력하세요.");
+			break;
+		case("멍멍"): case("멍멍아"): case("멍멍이"):
+			p[0].innerHTML="난 멍멍이가 아니야!";
+			alert("고양이 챗봇입니다.");
+			break;
+		case("불꺼줘"): case("불 꺼줘"):
+			p[0].innerHTML="잘 자";
+			img[0].src="chatbot/잘시간.jpg";
+			body[0].style.backgroundColor="#353535";
+			break;
+		case("불켜줘"): case("불 켜줘"):
+			p[0].innerHTML="이불 밖은 위험해...";
+			img[0].src="chatbot/이불.jpg";
+			body[0].style.backgroundColor="#FFFFFF";
+			break;
+		case("너 싫어"): case("저리가"):
+			p[0].innerHTML="너무해...";
+			img[0].src="chatbot/삐짐.jpg";
+			break;
+		case("잘있어"): case("갈게"):
+			p[0].innerHTML="진짜 갈거냥";
+			img[0].src="chatbot/갈거냥.jpg";
+			break;
+		case("게임하자"):
+			p[0].innerHTML="난 준비됐어!";
+			img[0].src="chatbot/게임.jpg";
+			break;
+		case("놀러가자"): case("운동가자"): 
+			p[0].innerHTML="따라와";
+			img[0].src="chatbot/자전거.jpg";
+			break;
+		case("뽀뽀"):
+			p[0].innerHTML="움먀";
+			img[0].src="chatbot/볼.jpg";
+			break;
+		default:
+			img[0].src="chatbot/cat.jpg";
+			p[0].innerHTML=value;
+			break;
+
+	}
 }
 document.getElementById('send').addEventListener('click', check_text);
