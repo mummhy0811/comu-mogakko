0c459ea88c7468e501aa0b36e1834ada5fb82140
mummhy0811 <mummhy0811@inu.ac.kr>
2022-02-16 16:00:51 +0900

    feat: 8일차 실습 추가 및 조건부렌더링 연습

--- 유튜브 클론코딩/App.js
+++ 유튜브 클론코딩/App.js
@@ -1,4 +1,5 @@
 import Layout from './components/shared/Layout';
+import styles from './App.css';
 import React, { useState } from 'react';
 
 
@@ -11,8 +12,8 @@ function App() {
   return(
     <Layout>
       <div>
+        { check ? <div className='liked'>좋아요 1개</div> : <div className='unliked'>좋아요 0개</div> }
         <button onClick={onClick}>좋아요</button>
-        { check && <div>+1</div> }
       </div>
     </Layout>
   );
