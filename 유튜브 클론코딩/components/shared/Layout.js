63e61598c3e015c8927d347db81a3a66af41fa26
mummhy0811 <mummhy0811@inu.ac.kr>
2022-02-22 16:56:34 +0900

    feat: 탐색 페이지 구현

--- 유튜브 클론코딩/components/shared/Layout.js
+++ 유튜브 클론코딩/components/shared/Layout.js
@@ -1,14 +1,22 @@
 import styles from './Layout.module.css';
 import Header from './Header';
 import Menu from './Menu';
+import { useState } from 'react';
 
-function Layout({ children }) {
+function Layout({ children, activeMenu }) {
+
+  const [showMenu, setMenu] = useState(true);
+
+  function onMenuClick(){
+    setMenu(!showMenu);
+  }
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header onMenuClick={onMenuClick}/>
       <div className={styles.layout}>
-        <Menu />
-        <div className={styles.contents}>{children}</div>
+        {showMenu ? <Menu activeMenu={activeMenu} /> : <div></div> }
+        <div className={ showMenu ? styles.contents : styles.noMenuContents}>{children}</div>
       </div>
     </div>
   );
