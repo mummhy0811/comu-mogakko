a557ae93432274faa409a91862df5c6ebf2e6091
mummhy0811 <mummhy0811@inu.ac.kr>
2022-02-24 21:21:07 +0900

    feat: 구독 페이지 작업 및 세부 표현 변경

---
+++ 유튜브 클론코딩/components/shared/HorizontalCard.js
@@ -0,0 +1,35 @@
+import moment from 'moment';
+import 'moment/locale/ko';
+import { ProcessViewCount } from '../../utils';
+import styles from './HorizontalCard.module.css';
+
+function HorizontalCard({ data }) {
+  return (
+    <a href={`https://www.youtube.com/watch?v=${data.id}`}>
+      <div className={styles.card}>
+        <img
+          className={styles.thumbnail}
+          src={data.thumbnail}
+          alt={`${data.title}의 썸네일`}
+        />
+        <div className={styles.info}>
+          <div className={styles.title}>{data.title}</div>
+          <div className={styles.meta}>
+            <a
+              href={`https://www.youtube.com/channel/${data.channelId}`}
+              className={styles.uploader}
+            >
+              {data.channelTitle}
+            </a>
+            <div className={styles.view}>
+              {ProcessViewCount(data.viewCount)}
+            </div>
+            <div className={styles.time}>{moment(data.date).fromNow()}</div>
+          </div>
+          <div className={styles.desc}>{data.description}</div>
+        </div>
+      </div>
+    </a>
+  );
+}
+export default HorizontalCard;
